f4bba5c6f68bb380e094ba09b4d1ac1e1d4b8085
matheusgondra <matheusgondra951@gmail.com>
2025-07-11 13:37:10 -0300

    test: ensure BirthDate throw a DomainError if the date is in the future

--- test/domain/entities/user/birth-date.spec.ts
+++ test/domain/entities/user/birth-date.spec.ts
@@ -2,7 +2,21 @@ import { BirthDate } from "../../../../src/domain/entities/user/birth-date";
 import { DomainError } from "../../../../src/domain/errors/domain-error";
 
 describe("BirthDate Entity", () => {
+	const domainError = new DomainError("Invalid birth date");
+
 	it("Should throw a DomainError if the date is invalid", () => {
-		expect(() => new BirthDate("invalid-date")).toThrow(new DomainError("Invalid birth date"));
+		const invalidDate = "invalid-date";
+		expect(() => new BirthDate(invalidDate)).toThrow(domainError);
+	});
+
+	it("Should throw a DomainError if the date is in the future", () => {
+		const SECOND = 1000;
+		const MINUTE = SECOND * 60;
+		const HOUR = MINUTE * 60;
+		const DAY = HOUR * 24;
+
+		const futureDate = new Date(Date.now() + DAY);
+
+		expect(() => new BirthDate(futureDate)).toThrow(domainError);
 	});
 });

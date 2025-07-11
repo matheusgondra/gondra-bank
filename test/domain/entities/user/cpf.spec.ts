47b81399076415651e42c7ae384a91a5d25d1148
matheusgondra <matheusgondra951@gmail.com>
2025-07-11 12:57:11 -0300

    test: ensure CPF throw a DomainError if cpf value is invalid sequence

--- test/domain/entities/user/cpf.spec.ts
+++ test/domain/entities/user/cpf.spec.ts
@@ -2,9 +2,13 @@ import { CPF } from "../../../../src/domain/entities/user/cpf";
 import { DomainError } from "../../../../src/domain/errors/domain-error";
 
 describe("CPF Entity", () => {
-	const invalidValue = "123";
-
 	it("Should throw a DomainError if cpf value length is less than 11", () => {
-		expect(() => new CPF(invalidValue)).toThrow(new DomainError("Invalid CPF length"));
+		const invalidValue = "123";
+		expect(() => new CPF(invalidValue)).toThrow(new DomainError("Invalid CPF"));
+	});
+
+	it("Should throw a DomainError if cpf value is invalid sequence", () => {
+		const invalidValue = "00000000000";
+		expect(() => new CPF(invalidValue)).toThrow(new DomainError("Invalid CPF"));
 	});
 });

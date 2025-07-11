1083b8748679cabb5882fb41426d081d8faf6cf2
matheusgondra <matheusgondra951@gmail.com>
2025-07-11 15:13:06 -0300

    test: ensure BirthDate.getValue returns the date value

--- test/domain/entities/user/birth-date.spec.ts
+++ test/domain/entities/user/birth-date.spec.ts
@@ -2,34 +2,46 @@ import { BirthDate } from "../../../../src/domain/entities/user/birth-date";
 import { DomainError } from "../../../../src/domain/errors/domain-error";
 
 describe("BirthDate Entity", () => {
-	const domainError = new DomainError("Invalid birth date");
-	const SECOND = 1000;
-	const MINUTE = SECOND * 60;
-	const HOUR = MINUTE * 60;
-	const DAY = HOUR * 24;
+	const validDate = "2000-11-23T00:00:00Z";
 
-	it("Should throw a DomainError if the date is invalid", () => {
-		const invalidDate = "invalid-date";
-		expect(() => new BirthDate(invalidDate)).toThrow(domainError);
+	describe("Constructor", () => {
+		const domainError = new DomainError("Invalid birth date");
+		const SECOND = 1000;
+		const MINUTE = SECOND * 60;
+		const HOUR = MINUTE * 60;
+		const DAY = HOUR * 24;
+
+		it("Should throw a DomainError if the date is invalid", () => {
+			const invalidDate = "invalid-date";
+			expect(() => new BirthDate(invalidDate)).toThrow(domainError);
+		});
+
+		it("Should throw a DomainError if the date is in the future", () => {
+			const futureDate = new Date(Date.now() + DAY);
+
+			expect(() => new BirthDate(futureDate)).toThrow(domainError);
+		});
+
+		it("Should throw a DomainError if the date is under 18 years old", () => {
+			const today = new Date();
+			const underEighteenYearsAgo = new Date(today.getFullYear() - 17, today.getMonth(), today.getDate());
+
+			expect(() => new BirthDate(underEighteenYearsAgo)).toThrow(domainError);
+		});
+
+		it("Should return a instance of BirthDate for a valid date", () => {
+			const instance = new BirthDate(validDate);
+
+			expect(instance).toBeInstanceOf(BirthDate);
+		});
 	});
 
-	it("Should throw a DomainError if the date is in the future", () => {
-		const futureDate = new Date(Date.now() + DAY);
+	describe("getValue", () => {
+		it("Should return the date value", () => {
+			const birthDate = new BirthDate(validDate);
+			const dateValue = new Date(validDate);
 
-		expect(() => new BirthDate(futureDate)).toThrow(domainError);
-	});
-
-	it("Should throw a DomainError if the date is under 18 years old", () => {
-		const today = new Date();
-		const underEighteenYearsAgo = new Date(today.getFullYear() - 17, today.getMonth(), today.getDate());
-
-		expect(() => new BirthDate(underEighteenYearsAgo)).toThrow(domainError);
-	});
-
-	it("Should return a instance of BirthDate for a valid date", () => {
-		const validDate = "2000-11-23T00:00:00Z";
-		const instance = new BirthDate(validDate);
-
-		expect(instance).toBeInstanceOf(BirthDate);
+			expect(birthDate.getValue()).toEqual(dateValue);
+		});
 	});
 });

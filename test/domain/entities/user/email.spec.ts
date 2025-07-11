4daf17a7f62b288f779c6d545ce9f3b378143a77
matheusgondra <matheusgondra951@gmail.com>
2025-07-11 13:24:25 -0300

    test: ensure Email.getValue return the email value

--- test/domain/entities/user/email.spec.ts
+++ test/domain/entities/user/email.spec.ts
@@ -2,22 +2,32 @@ import { Email } from "../../../../src/domain/entities/user/email";
 import { DomainError } from "../../../../src/domain/errors/domain-error";
 
 describe("Email Entity", () => {
-	const domainError = new DomainError("Invalid Email");
+	const validValue = "test@example.com";
 
-	it("Should throw a DomainError if email value is blank", () => {
-		const invalidValue = "";
-		expect(() => new Email(invalidValue)).toThrow(domainError);
+	describe("Constructor", () => {
+		const domainError = new DomainError("Invalid Email");
+
+		it("Should throw a DomainError if email value is blank", () => {
+			const invalidValue = "";
+			expect(() => new Email(invalidValue)).toThrow(domainError);
+		});
+
+		it("Should throw a DomainError if email value is not a valid email format", () => {
+			const invalidValue = "invalid-email";
+			expect(() => new Email(invalidValue)).toThrow(domainError);
+		});
+
+		it("Should return a valid Email instance if email value is valid", () => {
+			const email = new Email(validValue);
+
+			expect(email).toBeInstanceOf(Email);
+		});
 	});
 
-	it("Should throw a DomainError if email value is not a valid email format", () => {
-		const invalidValue = "invalid-email";
-		expect(() => new Email(invalidValue)).toThrow(domainError);
-	});
-
-	it("Should return a valid Email instance if email value is valid", () => {
-		const validValue = "test@example.com";
-		const email = new Email(validValue);
-
-		expect(email).toBeInstanceOf(Email);
+	describe("getValue", () => {
+		it("Should return the email value", () => {
+			const email = new Email(validValue);
+			expect(email.getValue()).toBe(validValue);
+		});
 	});
 });

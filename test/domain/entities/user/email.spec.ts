d9ae11b101f6820943d13855a01b8e398d5d6289
matheusgondra <matheusgondra951@gmail.com>
2025-07-11 13:20:13 -0300

    test: ensure Email throw a DomainError if email value is not a valid email format

--- test/domain/entities/user/email.spec.ts
+++ test/domain/entities/user/email.spec.ts
@@ -2,8 +2,15 @@ import { Email } from "../../../../src/domain/entities/user/email";
 import { DomainError } from "../../../../src/domain/errors/domain-error";
 
 describe("Email Entity", () => {
+	const domainError = new DomainError("Invalid Email");
+
 	it("Should throw a DomainError if email value is blank", () => {
 		const invalidValue = "";
-		expect(() => new Email(invalidValue)).toThrow(new DomainError("Invalid Email"));
+		expect(() => new Email(invalidValue)).toThrow(domainError);
+	});
+
+	it("Should throw a DomainError if email value is not a valid email format", () => {
+		const invalidValue = "invalid-email";
+		expect(() => new Email(invalidValue)).toThrow(domainError);
 	});
 });

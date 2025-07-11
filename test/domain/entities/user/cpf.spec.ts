059e2cdf0b4181ce3234eb708a0cf15d3197b4da
matheusgondra <matheusgondra951@gmail.com>
2025-07-11 13:03:36 -0300

    test: ensure CPF.getValue return the value of the CPF

--- test/domain/entities/user/cpf.spec.ts
+++ test/domain/entities/user/cpf.spec.ts
@@ -2,20 +2,31 @@ import { CPF } from "../../../../src/domain/entities/user/cpf";
 import { DomainError } from "../../../../src/domain/errors/domain-error";
 
 describe("CPF Entity", () => {
-	it("Should throw a DomainError if cpf value length is less than 11", () => {
-		const invalidValue = "123";
-		expect(() => new CPF(invalidValue)).toThrow(new DomainError("Invalid CPF"));
+	const validValue = "123.456.789-00";
+
+	describe("Constructor", () => {
+		it("Should throw a DomainError if cpf value length is less than 11", () => {
+			const invalidValue = "123";
+			expect(() => new CPF(invalidValue)).toThrow(new DomainError("Invalid CPF"));
+		});
+
+		it("Should throw a DomainError if cpf value is invalid sequence", () => {
+			const invalidValue = "00000000000";
+			expect(() => new CPF(invalidValue)).toThrow(new DomainError("Invalid CPF"));
+		});
+
+		it("Should create a valid instance", () => {
+			const cpf = new CPF(validValue);
+
+			expect(cpf).toBeInstanceOf(CPF);
+		});
 	});
 
-	it("Should throw a DomainError if cpf value is invalid sequence", () => {
-		const invalidValue = "00000000000";
-		expect(() => new CPF(invalidValue)).toThrow(new DomainError("Invalid CPF"));
-	});
+	describe("getValue", () => {
+		it("Should return the value of the CPF", () => {
+			const cpf = new CPF(validValue);
 
-	it("Should create a valid instance", () => {
-		const validValue = "123.456.789-00";
-		const cpf = new CPF(validValue);
-
-		expect(cpf).toBeInstanceOf(CPF);
+			expect(cpf.getValue()).toBe(validValue);
+		});
 	});
 });

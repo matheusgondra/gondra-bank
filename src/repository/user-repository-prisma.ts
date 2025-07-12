56afc419755c0e5603b293374b1e75bfbedad2d4
matheusgondra <matheusgondra951@gmail.com>
2025-07-12 14:14:25 -0300

    feat: ensure UserRepositoryPrisma returns a user when found by email or CPF

---
+++ src/repository/user-repository-prisma.ts
@@ -0,0 +1,22 @@
+import type { PrismaClient } from "@/../generated/prisma";
+import type { User } from "@/domain/entities/user/user";
+import type { UserRepository } from "@/interfaces/user-repository";
+import { UserMapper } from "@/mappers/user-mapper";
+
+export class UserRepositoryPrisma implements UserRepository {
+	constructor(private readonly db: PrismaClient) {}
+
+	async loadByEmailOrCpf(email: string, cpf: string): Promise<User | null> {
+		const user = await this.db.user.findFirst({
+			where: {
+				OR: [{ email: email, cpf: cpf }]
+			}
+		});
+
+		if (!user) {
+			return null;
+		}
+
+		return UserMapper.toDomain(user);
+	}
+}

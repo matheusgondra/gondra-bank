db0dad376dd22b56ae19695e43be8e4c13ab291f
matheusgondra <matheusgondra951@gmail.com>
2025-07-12 09:51:17 -0300

    feat: ensure InvestmentAccount.transfer throw a DomainError if amount is less than or equal to zero

--- src/domain/entities/account/investment-account.ts
+++ src/domain/entities/account/investment-account.ts
@@ -1,3 +1,13 @@
+import Decimal from "decimal.js";
+import { DomainError } from "@/domain/errors/domain-error";
 import { Account } from "./account";
 
-export class InvestmentAccount extends Account {}
+export class InvestmentAccount extends Account {
+	async transfer(destinationAccount: Account, amount: number): Promise<void> {
+		const amountDecimal = new Decimal(amount);
+		const isLessThanOrEqualToZero = amountDecimal.lessThanOrEqualTo(0);
+		if (isLessThanOrEqualToZero) {
+			throw new DomainError("Invalid transfer");
+		}
+	}
+}

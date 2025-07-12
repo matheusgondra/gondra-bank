a67e6a506591cd5d704766a1dbd60f2db793d88f
matheusgondra <matheusgondra951@gmail.com>
2025-07-12 13:50:38 -0300

    test: ensure LoginService call UserRepository with correct values

---
+++ test/services/login-service.spec.ts
@@ -0,0 +1,48 @@
+import { makeUserMock } from "@test/helpers/user-mock";
+import type { User } from "@/domain/entities/user/user";
+import type { UserRepository } from "@/interfaces/user-repository";
+import { LoginService } from "@/services/login-service";
+
+jest.mock("jsonwebtoken", () => ({
+	sign: () => "any_token"
+}));
+
+interface SutType {
+	sut: LoginService;
+	userRepositoryStub: UserRepository;
+}
+
+const makeUserRepositoryStub = (): UserRepository => {
+	class UserRepositoryStub implements UserRepository {
+		async loadByEmailOrCpf(email: string, cpf: string): Promise<User | null> {
+			return makeUserMock();
+		}
+	}
+
+	return new UserRepositoryStub();
+};
+
+const makeSut = (): SutType => {
+	const userRepositoryStub = makeUserRepositoryStub();
+	const sut = new LoginService(userRepositoryStub);
+
+	return {
+		sut,
+		userRepositoryStub
+	};
+};
+
+describe("LoginService", () => {
+	const email = "any@mail.com";
+	const password = "StrongPassword@123";
+	const cpf = "12345678901";
+
+	it("Should call UserRepository with correct values", async () => {
+		const { sut, userRepositoryStub } = makeSut();
+		const loadByEmailOrCpfSpy = jest.spyOn(userRepositoryStub, "loadByEmailOrCpf");
+
+		await sut.login(email, password, cpf);
+
+		expect(loadByEmailOrCpfSpy).toHaveBeenCalledWith(email, cpf);
+	});
+});

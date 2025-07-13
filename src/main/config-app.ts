3468aea7a38657af022bcc3f6841ae13c6afa995
matheusgondra <matheusgondra951@gmail.com>
2025-07-13 12:24:30 -0300

    chore: cofing cors

--- src/main/config-app.ts
+++ src/main/config-app.ts
@@ -4,6 +4,12 @@ import { routes } from "./routes";
 const app = express();
 
 app.use(express.json());
+app.use((req, res, next) => {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Methods", "*");
+	res.header("Access-Control-Allow-Headers", "*");
+	next();
+});
 
 routes(app);
 

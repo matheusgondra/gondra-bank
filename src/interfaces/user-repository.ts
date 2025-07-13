6aae9f20102db55dd0203467706d99a0b587e87f
matheusgondra <matheusgondra951@gmail.com>
2025-07-12 23:11:35 -0300

    feat: ensure UserRepositoryPrisma.loadById returns a user when found by ID

--- src/interfaces/user-repository.ts
+++ src/interfaces/user-repository.ts
@@ -2,4 +2,5 @@ import type { User } from "@/domain/entities/user/user";
 
 export interface UserRepository {
 	loadByEmailOrCpf(email: string, cpf: string): Promise<User | null>;
+	loadById(id: number): Promise<User | null>;
 }
